dae5533677035d37a6fffe4c753180ae3a3007af
Jonathan Kim <jkimbo@gmail.com>
2011-08-07 22:04:30 +0100

    Added some info to config.sample.php

--- inc/config.sample.php
+++ inc/config.sample.php
@@ -1,4 +1,8 @@
 <?php
+    /*
+     * Create a config.local.php with the information below to run on a local dev machine
+     */
+
     $db = "DB_TABLE";
     $host = "localhost";
     $user = "DB_USER";
@@ -6,9 +10,12 @@
     $cid = mysql_connect($host,$user,$pass);
     $dbok = mysql_select_db($db,$cid);
 
-	define('STANDARD_URL','http://felixonline.co.uk/');
-	define('BASE_URL','http://felixonline.co.uk/');
-	define('ADMIN_URL','http://felixonline.co.uk/engine/');
+    /*
+     * Change these urls to your local versions, e.g http://localhost/felix
+     */
+    define('STANDARD_URL','http://felixonline.co.uk/');
+    define('BASE_URL','http://felixonline.co.uk/');
+    define('ADMIN_URL','http://felixonline.co.uk/engine/');
 
-	define('DEVELOPMENT_FLAG', false); // if set to true css and js won't be minified etc..
+    define('DEVELOPMENT_FLAG', false); // if set to true css and js won't be minified etc..
 ?>
