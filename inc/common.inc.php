b31438d5557a236f2de61c29c0033aa967e28cbb
Jonathan Kim <jkimbo@gmail.com>
2011-11-18 16:09:02 +0000

    Some intial authentication commenting and auth is now in another file

--- inc/common.inc.php
+++ inc/common.inc.php
@@ -334,17 +334,17 @@ function curPageURL() {
 }
 
 function curPageURLSecure() {
- $pageURL = 'https://';
- if ($_SERVER["SERVER_PORT"] != "80") {
-  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
- } else {
-  $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
- }
- return $pageURL;
+    $pageURL = 'https://';
+    if ($_SERVER["SERVER_PORT"] != "80") {
+        $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+    } else {
+        $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+    }
+    return $pageURL;
 }
 
 function curPageURLNonSecure() {
- return 'http://'.$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+    return 'http://'.$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
 }
 
 function login($username) {
