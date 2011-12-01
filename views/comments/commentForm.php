e697acfbe4dc3af188fda9945dc122a2bb3594b9
Jonathan Kim <jkimbo@gmail.com>
2011-12-01 22:22:09 +0000

    Moved commenting policy to own file

--- views/comments/commentForm.php
+++ views/comments/commentForm.php
@@ -66,18 +66,5 @@
         <input type="submit" value="Post your comment" id="submit" name="<?php if($uname) echo 'articlecomment'; else echo 'articlecomment_ext';?>"/>
     </form>
     <!-- Commenting Policy -->
-    <div id="commentPolicy">
-        <div id="commentPolicyPopUp">
-            <h3>Commenting Policy</h3> 
-            <p>Felix is fully supportive of fierce and frank discussions. We will generally allow comments that are on-topic and avoid personal attacks; criticising a someone's decisions is allowed, criticising their weight, for example, is not.</p>
-            <p>Comments may be deleted/rejected if they:</p>
-            <ul>
-                <li>are off-topic</li>
-                <li>contain ad-hominem attacks</li>
-                <li>are potentially libellous</li>
-                <li>use excessively foul language</li>
-                <li>are obviously spam</li>
-            </ul>
-        </div>
-    </div>
+    <?php include('views/comments/commentPolicy.php'); ?>
 </div>
