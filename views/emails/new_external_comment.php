09db7f616d7933bc850edbf3bcef80e83c7bc4ec
Jonathan Kim <jkimbo@gmail.com>
2011-12-04 00:54:45 +0000

    Added a links to new external email template

--- views/emails/new_external_comment.php
+++ views/emails/new_external_comment.php
@@ -6,12 +6,12 @@
  */
 ?>
 <p>A new comment on the post "<?php echo get_article_title($comment->getArticle()); ?>" is waiting for your approval. </br>
-<?php echo full_article_url($comment->getArticle()); ?>
+<a href="<?php echo full_article_url($comment->getArticle()); ?>"/><?php echo full_article_url($comment->getArticle()); ?></a>
 </p>
 
 <p>
     Author: <?php echo stripslashes($comment->getName()); ?> (IP: <?php echo $_SERVER['REMOTE_ADDR'];?>)</br>
-    Whois: http://ip-whois-lookup.com/lookup.php?ip=<?php echo $_SERVER['REMOTE_ADDR']; ?>
+    Whois: <a href="http://ip-whois-lookup.com/lookup.php?ip=<?php echo $_SERVER['REMOTE_ADDR']; ?>"/>http://ip-whois-lookup.com/lookup.php?ip=<?php echo $_SERVER['REMOTE_ADDR']; ?></a>
 </p>
 
 <p>
@@ -20,11 +20,12 @@
 </p>
 
 <p>
-    Approve it: <?php echo STANDARD_URL."engine/?page=comment&action=approve&c=".$this->getID(); ?></br>
-    Trash it: <?php echo STANDARD_URL."engine/?page=comment&action=trash&c=".$this->getID(); ?></br>
-    Spam it: <?php echo STANDARD_URL."engine/?page=comment&action=spam&c=".$this->getID();?>
+    Approve it: <a href="<?php echo STANDARD_URL."engine/?page=comment&action=approve&c=".$this->getID(); ?>"><?php echo STANDARD_URL."engine/?page=comment&action=approve&c=".$this->getID(); ?></a></br>
+    Trash it: <a href="<?php echo STANDARD_URL."engine/?page=comment&action=trash&c=".$this->getID(); ?>"><?php echo STANDARD_URL."engine/?page=comment&action=trash&c=".$this->getID(); ?></a></br>
+    Spam it: <a href="<?php echo STANDARD_URL."engine/?page=comment&action=spam&c=".$this->getID();?>"><?php echo STANDARD_URL."engine/?page=comment&action=spam&c=".$this->getID();?></a>
+
 </p>
 
-<p>There are <?php echo get_comments_to_approve(); ?> comment(s) waiting to be approved. View them here: <?php echo BASE_URL."engine/?page=comment"; ?></p>
+<p>There are <?php echo get_comments_to_approve(); ?> comment(s) waiting to be approved. View them here: <a href="<?php echo STANDARD_URL."engine/?page=comment"; ?>"><?php echo STANDARD_URL."engine/?page=comment"; ?></a></p>
 
 <p>Felix Online</p>
