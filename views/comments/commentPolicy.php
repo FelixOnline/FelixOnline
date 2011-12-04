21634ff87cbdf801a63bb17829d2a1ac18566683
Jonathan Kim <jkimbo@gmail.com>
2011-12-04 11:10:15 +0000

    Fixed typo in commenting policy

--- views/comments/commentPolicy.php
+++ views/comments/commentPolicy.php
@@ -1,7 +1,7 @@
 <div id="commentPolicy">
     <div id="commentPolicyPopUp">
         <h3>Commenting Policy</h3> 
-        <p>Felix is fully supportive of fierce and frank discussions. We will generally allow comments that are on-topic and avoid personal attacks; criticising a someone's decisions is allowed, criticising their weight, for example, is not.</p>
+        <p>Felix is fully supportive of fierce and frank discussions. We will generally allow comments that are on-topic and avoid personal attacks; criticising someone's decision is allowed, criticising their weight, for example, is not.</p>
         <p>Comments may be deleted/rejected if they:</p>
         <ul>
             <li>are off-topic</li>
