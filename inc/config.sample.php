5a60fbc0b8b097649057d116a00a1923e6e7ab6e
Jonathan Kim <jkimbo@gmail.com>
2011-12-11 16:46:54 +0000

    Updated config.sample.php with ezsql config

--- inc/config.sample.php
+++ inc/config.sample.php
@@ -11,15 +11,22 @@
     $dbok = mysql_select_db($dbname,$cid);
 
     /* Initialise ezSQL database connection */
-    $db = new ezSQL_mysql($user,$pass,$dbname,$host);
+    $db = new ezSQL_mysql();
+    $db->quick_connect($user,$pass,$dbname,$host);
+
+    /* Set settings for caching (turned off by defualt) */
+	// Cache expiry
+	$db->cache_timeout = 24; // Note: this is hours
+	$db->use_disk_cache = true;
+	$db->cache_dir = 'inc/ezsql_cache'; // Specify a cache dir. Path is taken from calling script
 
     /* Forces charset to be utf8 */
-    mysql_set_charset('utf8',$cid);
+    mysql_set_charset('utf8',$db->dbh);
 
     /* turn off error reporting */
-    error_reporting(0);
+    //error_reporting(0);
     /* to turn on error reporting uncomment line: */
-    //error_reporting(E_ERROR | E_WARNING | E_PARSE);
+    error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
     /*
      * Change these urls to your local versions, e.g http://localhost/felix
@@ -30,6 +37,6 @@
     define('AUTHENTICATION_SERVER','localhost'); // authentication server
     define('AUTHENTICATION_PATH','http://localhost/felix/'); // authentication path
 
-    define('PRODUCTION_FLAG', true); // if set to true css and js will be minified etc..
-    define('LOCAL', false); // if true then site is hosted locally - don't use pam_auth etc. 
+    define('PRODUCTION_FLAG', false); // if set to true css and js will be minified etc..
+    define('LOCAL', true); // if true then site is hosted locally - don't use pam_auth etc. 
 ?>
