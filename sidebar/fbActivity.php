05a8fc76615c595d9d477c2461a70da5f3b306b8
Philip Kent <kentphilip@gmail.com>
2012-02-05 12:28:23 +0000

    Update facebook social feed box

--- sidebar/fbActivity.php
+++ sidebar/fbActivity.php
@@ -1,5 +1,4 @@
 <div id="fbActivity">
 	<h3>Recent Activity</h3>
-	<iframe src="http://www.facebook.com/plugins/activity.php?site=felixonline.co.uk&amp;width=300&amp;height=300&amp;header=false&amp;colorscheme=light&amp;font=arial&amp;border_color=%23BEBEBE&amp;recommendations=false" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:300px; height:300px;" allowTransparency="true"></iframe>
-	<!--<fb:activity site="felixonline.co.uk" width="300" height="300" header="false" font="arial" border_color="#BEBEBE" recommendations="false"></fb:activity>-->
+	<fb:activity site="felixonline.co.uk" app_id="200482590030408" action="like" width="300" height="300" header="false" border_color="#BEBEBE" font="arial" recommendations="false"></fb:activity>
 </div>
