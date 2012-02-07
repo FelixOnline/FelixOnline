ed1461ce5005c3bf6a0b29108fc9f9ead6804166
Jonathan Kim <jkimbo@gmail.com>
2012-02-07 18:26:35 +0000

    Added a cache class and tested it on frontpage

--- themes/classic/frontpage.php
+++ themes/classic/frontpage.php
@@ -36,6 +36,11 @@ $timing->log('after header');
     <!-- Front page articles -->
     <div class="grid_8 pull_4 featCont layout1">
         <?php
+            // Start caching
+            $cache = new Cache('frontpage');
+            if($cache->start()) {
+        ?>
+        <?php
             // Section a
             $sql = "SELECT 
                 `1` as one, 
@@ -382,8 +387,9 @@ $timing->log('after header');
         </div>
         <!-- End of featured articles -->
         
-        <?php $timing->log('end of frontpage articles'); ?>
+        <?php } $cache->stop(); ?>
 
+        <?php $timing->log('end of frontpage articles'); ?>
         <!-- Editorial -->
         <div class="grid_4 alpha commentBox">
             <div class="border">
