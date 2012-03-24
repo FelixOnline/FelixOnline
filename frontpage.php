70f5095598ce010f951826e1f44bf2503fe915db
Jonathan Kim <jkimbo@gmail.com>
2012-03-24 01:52:46 +0000

    Added tedx sidebar box

--- frontpage.php
+++ frontpage.php
@@ -12,7 +12,8 @@
 		<!-- Sidebar -->
 		<div class="sidebar grid_4 push_8">
 			<?php
-                                include_once('sidebar/sexsurvey.php');
+                include_once('sidebar/tedx.php');
+                include_once('sidebar/sexsurvey.php');
 				include_once('sidebar/fbLikeBox.php');
                 $timing->log('after fblikebox');
 				include_once('sidebar/mediaBox.php');
---
+++ sidebar/tedx.php
@@ -0,0 +1,6 @@
+<div id="tedx">
+    <a href="http://felixonline.co.uk/tedx/">
+        <img src="<?php echo STANDARD_URL; ?>img/tedx.png" width="250px" title="TEDxImperialCollege"> 
+    </a>
+    <p>TED is coming to Imperial College today! Follow our liveblog of all the days events right here: <a href="http://felixonline.co.uk/tedx/" title="TEDxImperialCollege">felixonline.co.uk/tedx</a></p>
+</div>
