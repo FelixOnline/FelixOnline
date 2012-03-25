ed88134bbef156e0611801aa6a5bd509b61e8ccc
Jonathan Kim <jkimbo@gmail.com>
2012-03-25 20:37:56 +0100

    Removed unnecessary construct statement

--- themes/classic/core/theme-classic.class.php
+++ themes/classic/core/theme-classic.class.php
@@ -1,9 +1,6 @@
 <?php
 
 class ThemeClassic extends Theme {
-    function __construct($name) {
-        parent::__construct($name);
-    }
 
     public function foobar() {
         return 'Foo Bar';
