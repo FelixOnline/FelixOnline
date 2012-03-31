3a34187f301a2e8e4a05c28098ac457d9bf45c66
Jonathan Kim <jkimbo@gmail.com>
2012-03-31 12:02:15 +0100

    Simplified logic for caching comments

--- themes/classic/article.php
+++ themes/classic/article.php
@@ -181,7 +181,7 @@ $theme->render('header', $header);
         <div class="grid_8 comments" id="commentHeader">
             <?php 
                 $cache = new Cache('comment-'.$article->getId());
-                if($currentuser->isLoggedIn() || (!$currentuser->isLoggedIn() && $cache->start())) {
+                if($currentuser->isLoggedIn() || $cache->start()) {
             ?>
             <h3>Comments <span>(<?php echo $article->getNumComments().' comment'.($article->getNumComments() != 1 ? 's' : '');?>)</span></h3>
             <!-- Comments container -->
