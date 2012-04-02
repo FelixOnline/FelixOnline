b10c6099130e056cdd588f5ae97e42c0f951b65f
Jonathan Kim <jkimbo@gmail.com>
2012-04-02 12:09:16 +0100

    Added update user info

--- themes/classic/user.php
+++ themes/classic/user.php
@@ -73,6 +73,7 @@ $theme->render('header', $header);
                     </span>
                     <span class="loading">Saving...</span>
                 </h2>
+                <?php var_dump($user->getInfo()); ?>
 				<ul id="userInfo">
 					<li><?php //echo $info[0]; ?></li>
 					<li><?php //echo $info[1]; ?></li>
