9c2071a000f086a13b6969d54d0269a59590b0c7
Jonathan Kim <jonathan.kim@fusepump.com>
2013-06-22 19:43:25 +0100

    Removed sql queries from frontpage #61

--- bootstrap.php
+++ bootstrap.php
@@ -21,6 +21,7 @@ require_once(BASE_DIRECTORY.'/inc/is_email.inc.php');
  */
 require_once(BASE_DIRECTORY.'/core/baseModel.class.php');
 require_once(BASE_DIRECTORY.'/core/user.class.php');
+require_once(BASE_DIRECTORY.'/core/frontpage.class.php');
 foreach (glob(BASE_DIRECTORY.'/core/*.php') as $filename) {
     require_once($filename);
 }
