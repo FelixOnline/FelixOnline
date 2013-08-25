398b0a5087447b13995119fb33083c8a80ab68f6
Philip Kent <philip.kent@me.com>
2013-08-25 16:35:37 +0100

    Remove SQL from the most popular box (see #61)

--- core/article.class.php
+++ core/article.class.php
@@ -444,5 +444,50 @@ class Article extends BaseModel {
 	public function print_this() {
 		print_r($this);
 	}
+
+	public static function getMostPopular($number_to_get) {
+		global $db;
+		
+		$sql = "SELECT DISTINCT article AS id,COUNT(article) AS c 
+			FROM (
+				SELECT article FROM article_visit AS av 
+				INNER JOIN article AS a 
+				ON (av.article=a.id) 
+				WHERE a.published IS NOT NULL 
+				ORDER BY timestamp DESC LIMIT 500
+			) AS t GROUP BY article ORDER BY c DESC LIMIT ".$number_to_get;
+		
+		return $db->get_results($sql);	
+	}
+	
+	public static function getMostCommented($threshold, $number_to_get) {
+		global $db;
+		
+		$sql = "SELECT article AS id,SUM(count) AS count 
+		FROM (
+				(SELECT c.article,COUNT(*) AS count 
+				FROM `comment` AS c 
+				INNER JOIN `article` AS a ON (c.article=a.id) 
+				WHERE c.`active`=1 
+				AND timestamp>(DATE_SUB(NOW(),INTERVAL ".$threshold." day)) 
+				AND a.published<NOW() 
+				GROUP BY article 
+				ORDER BY timestamp DESC 
+				LIMIT 20) 
+			UNION ALL 
+				(SELECT ce.article,COUNT(*) AS count 
+				FROM `comment_ext` AS ce 
+				INNER JOIN `article` AS a ON (ce.article=a.id) 
+				WHERE ce.`active`=1 
+				AND pending=0 
+				AND timestamp>(DATE_SUB(NOW(),INTERVAL ".$threshold." day)) 
+				AND a.published<NOW() 
+				GROUP BY article 
+				ORDER BY timestamp DESC)
+		) AS t 
+		GROUP BY article 
+		ORDER BY count DESC, article DESC LIMIT ".$number_to_get; // go for most recent comments instead
+		return $db->get_results($sql);
+	}
 }
 
--- themes/classic/sidebar/mostPopular.php
+++ themes/classic/sidebar/mostPopular.php
@@ -10,15 +10,7 @@
 	</ul>
 	<div class="mostPopularTab" id="mostPopRead">
 		<?php 
-			$sql = "SELECT DISTINCT article AS id,COUNT(article) AS c 
-			FROM (
-				SELECT article FROM article_visit AS av 
-				INNER JOIN article AS a 
-				ON (av.article=a.id) 
-				WHERE a.published IS NOT NULL 
-				ORDER BY timestamp DESC LIMIT 500
-			) AS t GROUP BY article ORDER BY c DESC LIMIT 5";
-			$viewed_articles = $db->get_results($sql);
+			$viewed_articles = Article::getMostPopular(POPULAR_ARTICLES);
 			if(!is_null($viewed_articles)) {
 				echo '<ol>';
 				foreach($viewed_articles as $object) {
@@ -37,31 +29,7 @@
 	</div>
 	<div class="mostPopularTab" id="mostPopComment">
 		<?php
-			$sql = "SELECT article AS id,SUM(count) AS count 
-			FROM (
-					(SELECT c.article,COUNT(*) AS count 
-					FROM `comment` AS c 
-					INNER JOIN `article` AS a ON (c.article=a.id) 
-					WHERE c.`active`=1 
-					AND timestamp>(DATE_SUB(NOW(),INTERVAL ".MOST_POPULAR_INTERVAL." day)) 
-					AND a.published<NOW() 
-					GROUP BY article 
-					ORDER BY timestamp DESC 
-					LIMIT 20) 
-				UNION ALL 
-					(SELECT ce.article,COUNT(*) AS count 
-					FROM `comment_ext` AS ce 
-					INNER JOIN `article` AS a ON (ce.article=a.id) 
-					WHERE ce.`active`=1 
-					AND pending=0 
-					AND timestamp>(DATE_SUB(NOW(),INTERVAL ".MOST_POPULAR_INTERVAL." day)) 
-					AND a.published<NOW() 
-					GROUP BY article 
-					ORDER BY timestamp DESC)
-			) AS t 
-			GROUP BY article 
-			ORDER BY count DESC, article DESC LIMIT ".POPULAR_ARTICLES; // go for most recent comments instead
-			$popular_articles = $db->get_results($sql);
+			$popular_Article = Article::getMostCommented(MOST_POPULAR_INTERVAL, POPULAR_ARTICLES);
 			if(!is_null($popular_articles)) {
 				echo '<ol>';
 				foreach ($popular_articles as $object) {
