ede879824140d7274c5bbdecc39e275ca27d94ad
Philip Kent <philip.kent@me.com>
2013-12-19 00:42:05 +0000

    Automatically load SSL resources if needed for externals

--- themes/classic/sidebar/fbLikeBox.php
+++ themes/classic/sidebar/fbLikeBox.php
@@ -1,5 +1,5 @@
 <div id="fbLikeBox">
-	<iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2FFelixImperial&amp;width=300&amp;colorscheme=light&amp;show_faces=false&amp;stream=false&amp;header=false&amp;height=62" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:300px; height:62px;" allowTransparency="true"></iframe>
+	<iframe src="//www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2FFelixImperial&amp;width=300&amp;colorscheme=light&amp;show_faces=false&amp;stream=false&amp;header=false&amp;height=62" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:300px; height:62px;" allowTransparency="true"></iframe>
 	<!--<fb:like-box href="http://www.facebook.com/FelixImperial" width="300" show_faces="false" stream="false" header="true"></fb:like-box>-->
 </div>
 <?php $timing->log('after fblikebox'); ?>
