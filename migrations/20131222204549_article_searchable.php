d8cf4f401f6f036869998c4558db28b656ee8527
Jonathan Kim <jkimbo@gmail.com>
2014-01-06 14:24:47 +0000

    Modified migration to set default value of searchable to 1

--- migrations/20131222204549_article_searchable.php
+++ migrations/20131222204549_article_searchable.php
@@ -5,17 +5,26 @@ use Phinx\Migration\AbstractMigration;
 class ArticleSearchable extends AbstractMigration
 {
     /**
-     * Change Method.
+     * Up Method.
 	 */
-    public function change()
+    public function up()
     {
 		$table = $this->table('article');
 		$table->addColumn('searchable', 'boolean', array(
 			'comment' => 'Should web crawlers index this?',
 			'null' => false,
 			'after' => 'hidden',
+			'default' => 1,
 		))
 		->addIndex(array('searchable'))
 		->save();
+    }
+
+    /**
+     * Down Method.
+	 */
+    public function down()
+    {
+		$this->execute('ALTER TABLE article DROP searchable');
     }
 }
