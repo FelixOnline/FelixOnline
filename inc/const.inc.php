c5c2601c81ba513b5f27257ec155cd806ab1168a
Jonathan Kim <jkimbo@gmail.com>
2014-01-10 14:15:13 +0000

    Fix redirect if login fails and changed my email address for external comment notifications

--- inc/const.inc.php
+++ inc/const.inc.php
@@ -107,7 +107,7 @@
 	define('EMAIL_COMMENT_SUBJECT_PREFIX','New comment: ');
 	define('EMAIL_COMMENT_AUTHOR',true);
 	define('EMAIL_COMMENT_COMMENTERS',true);
-	define('EMAIL_EXTCOMMENT_NOTIFYADDR','jk708@ic.ac.uk, felix@imperial.ac.uk'); // comma-separated list of addresses to notify when a new external comment needs approval
+	define('EMAIL_EXTCOMMENT_NOTIFYADDR', 'jkimbo@gmail.com, felix@imperial.ac.uk'); // comma-separated list of addresses to notify when a new external comment needs approval
 	if(!defined('EMAIL_ERRORS'))		define('EMAIL_ERRORS', 'jkimbo@gmail.com,philip.kent@me.com'); // people to email on errors - separate with commas only (no spaces)
 
 	/* SITE CONSTANTS */
