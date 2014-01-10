92f43e5a09a5914ca3f0d34c99e5704fa8169682
Philip Kent <philip.kent@me.com>
2014-01-10 23:26:14 +0000

    Reindenting

--- errors/error.php
+++ errors/error.php
@@ -92,109 +92,109 @@
 				}
 			}
 
-				if(!LOCAL && $notify) {
-					$notify = true;
-					if(file_exists(dirname(__FILE__).'/../emails/fatal_next_notify')) {
-						$next_notify = (int) file_get_contents(dirname(__FILE__).'/../emails/fatal_next_notify');
-						if($next_notify > time()) {
-							// We have notified already, don't notify again
-							$notify = false;
-						}
-					}
-					$to = explode(',', EMAIL_ERRORS);
-					$subject = 'Felix Online error';
-					$message = "An error has occured on Felix Online\n";
-					$message .= "Details on the error is below. If there are multiple errors, the first is the main one, the second prevented the usual error page from being shown\n\n";
-					
-					$exceptions = array($prior_exception, $e);
-
-					foreach($exceptions as $exception) {
-						if ($exception) {
-							$data = array();
-							$data['Details'] = $exception->getMessage();
-							
-							if(!($exception instanceof Exception) && $exception->getUser()->getUser() instanceof User) {
-								$username = $exception->getUser()->getUser()->getName();
-							} else {
-								$username = 'Unauthenticated';
-							}
-							switch ($exception->getCode()) {
-								case EXCEPTION_ERRORHANDLER:
-									$header = 'Internal error';
-									$data['File'] = $exception->getErrorFile();
-									$data['Line'] = $exception->getErrorLine();
-									break;
-								case EXCEPTION_GLUE:
-									$header = 'Misconfigured glue';
-									$data['URL'] = $exception->getUrl();
-									$data['Class requested'] = $exception->getItem();
-									$data['Method requested'] = $exception->getVerb();
-									break;
-								case EXCEPTION_GLUE_URL:
-									$header = 'URL is not valid';
-									$data['URL'] = $exception->getUrl();
-									break;
-								case EXCEPTION_IMAGE_NOTFOUND:
-									$dimensions = $exception->getImageDimensions();
-									$header = 'Image could not be found';
-									$data['Containing page'] = $exception->getPage();
-									$data['Image URL'] = $exception->getImageUrl();
-									$data['Requested dimensions'] = $dimensions['width'].'x'.$dimensions['height'];
-									break;
-								case EXCEPTION_MODEL:
-									$header = 'Misconfigured model';
-									$data['Item type'] = $exception->getClass();
-									$data['Item identifier'] = $exception->getItem();
-									$data['Action'] = $exception->getVerb();
-									$data['Property'] = $exception->getProperty();
-									break;
-								case EXCEPTION_MODEL_NOTFOUND:
-									$header = 'Item is not in database';
-									$data['Item type'] = $exception->getClass();
-									$data['Item identifier'] = $exception->getItem();
-									break;
-								case EXCEPTION_VIEW_NOTFOUND:
-									$header = 'Template does not exist';
-									$data['Template'] = $exception->getView();
-									break;
-								default:
-									$header = 'Internal exception';
-									break;
-							}
-
-							$data['Exception file'] = $exception->getFile();
-							$data['Exception line'] = $exception->getLine();
-
-							$message .= $header."\n";
-							$message .= "URL accessed: http://".STANDARD_SERVER."/".$_SERVER["REQUEST_URI"]."\n";
-							$message .= "Username: ".$username."\n";
-							foreach($data as $name => $value) {
-								$message .= $name.": ".$value."\n";
-							}
-							$message .= "\nBacktrace:\n";
-							$message .= $exception->getTraceAsString();
-							$message .= "\n\n\n";
-						}
-					}
-
-					//$message = wordwrap($message, 70);
-					if($notify) {
-						$status = false;
-
-						foreach($to as $addressee) {
-							$successful = mail($addressee, $subject, $message);
-
-							if($successful) {
-								$status = true;
-							}
-						}
-						
-						if(!$status) {
-							echo 'Notification failed';
-						} else {
-							file_put_contents(dirname(__FILE__).'/../emails/fatal_next_notify', time() + 900);
-						}
+			if(!LOCAL && $notify) {
+				$notify = true;
+				if(file_exists(dirname(__FILE__).'/../emails/fatal_next_notify')) {
+					$next_notify = (int) file_get_contents(dirname(__FILE__).'/../emails/fatal_next_notify');
+					if($next_notify > time()) {
+						// We have notified already, don't notify again
+						$notify = false;
 					}
 				}
-				?>
-			</div>
+				$to = explode(',', EMAIL_ERRORS);
+				$subject = 'Felix Online error';
+				$message = "An error has occured on Felix Online\n";
+				$message .= "Details on the error is below. If there are multiple errors, the first is the main one, the second prevented the usual error page from being shown\n\n";
+				
+				$exceptions = array($prior_exception, $e);
+
+				foreach($exceptions as $exception) {
+					if ($exception) {
+						$data = array();
+						$data['Details'] = $exception->getMessage();
+						
+						if(!($exception instanceof Exception) && $exception->getUser()->getUser() instanceof User) {
+							$username = $exception->getUser()->getUser()->getName();
+						} else {
+							$username = 'Unauthenticated';
+						}
+						switch ($exception->getCode()) {
+							case EXCEPTION_ERRORHANDLER:
+								$header = 'Internal error';
+								$data['File'] = $exception->getErrorFile();
+								$data['Line'] = $exception->getErrorLine();
+								break;
+							case EXCEPTION_GLUE:
+								$header = 'Misconfigured glue';
+								$data['URL'] = $exception->getUrl();
+								$data['Class requested'] = $exception->getItem();
+								$data['Method requested'] = $exception->getVerb();
+								break;
+							case EXCEPTION_GLUE_URL:
+								$header = 'URL is not valid';
+								$data['URL'] = $exception->getUrl();
+								break;
+							case EXCEPTION_IMAGE_NOTFOUND:
+								$dimensions = $exception->getImageDimensions();
+								$header = 'Image could not be found';
+								$data['Containing page'] = $exception->getPage();
+								$data['Image URL'] = $exception->getImageUrl();
+								$data['Requested dimensions'] = $dimensions['width'].'x'.$dimensions['height'];
+								break;
+							case EXCEPTION_MODEL:
+								$header = 'Misconfigured model';
+								$data['Item type'] = $exception->getClass();
+								$data['Item identifier'] = $exception->getItem();
+								$data['Action'] = $exception->getVerb();
+								$data['Property'] = $exception->getProperty();
+								break;
+							case EXCEPTION_MODEL_NOTFOUND:
+								$header = 'Item is not in database';
+								$data['Item type'] = $exception->getClass();
+								$data['Item identifier'] = $exception->getItem();
+								break;
+							case EXCEPTION_VIEW_NOTFOUND:
+								$header = 'Template does not exist';
+								$data['Template'] = $exception->getView();
+								break;
+							default:
+								$header = 'Internal exception';
+								break;
+						}
+
+						$data['Exception file'] = $exception->getFile();
+						$data['Exception line'] = $exception->getLine();
+
+						$message .= $header."\n";
+						$message .= "URL accessed: http://".STANDARD_SERVER."/".$_SERVER["REQUEST_URI"]."\n";
+						$message .= "Username: ".$username."\n";
+						foreach($data as $name => $value) {
+							$message .= $name.": ".$value."\n";
+						}
+						$message .= "\nBacktrace:\n";
+						$message .= $exception->getTraceAsString();
+						$message .= "\n\n\n";
+					}
+				}
+
+				//$message = wordwrap($message, 70);
+				if($notify) {
+					$status = false;
+
+					foreach($to as $addressee) {
+						$successful = mail($addressee, $subject, $message);
+
+						if($successful) {
+							$status = true;
+						}
+					}
+					
+					if(!$status) {
+						echo '<p><b>We couldn\'t notify the Felix Online developers, please contact us at felix@imperial.ac.uk for further assistance.</b></p>';
+					} else {
+						file_put_contents(dirname(__FILE__).'/../emails/fatal_next_notify', time() + 900);
+					}
+				}
+			}
+			?>
+		</div>
