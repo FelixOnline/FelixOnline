73641c9b054aa15b8e584e40a481486cf6da130b
Joe <entrepreneurj@gmail.com>
2014-03-12 04:32:39 +0000

    Changes varsity image, page structure and adds STOIC embed feed

--- themes/classic/blog-varsity-2014.php
+++ themes/classic/blog-varsity-2014.php
@@ -37,15 +37,15 @@
 			</div>
 		</div>
 		<div class="masthead">
-			<img src="<?php echo STANDARD_URL . "themes/" . THEME_NAME . "/img/varsity-basketball.jpg"; ?>"/>
+			<img src="<?php echo STANDARD_URL . "themes/" . THEME_NAME . "/img/varsity-2014.jpg"; ?>"/>
 		</div>
 
 		<div class="row">
-			<div class="col-md-6 col-md-offset-1 info clearfix">
+			<div class="col-md-12 info clearfix">
 				<h3>Live Reporting</h3>
 				<div class="info-left">
 					<div class="reporters">
-						By <a href="<?php echo STANDARD_URL . 'user/kmw13'; ?>">Kunal Wagle</a>
+						By <a href="<?php echo STANDARD_URL . 'user/kmw13'; ?>">Kunal Wagle (Sports)</a>, <a href="<?php echo STANDARD_URL . 'user/jal08'; ?>">Joe Letts (Editor)</a>
 					</div>
 					<div class="status">
 						<div id="disconnected">
@@ -58,13 +58,16 @@
 				</div>
 				<div class="info-right">
 					<div class="fb-like" data-href="http://felixonline.co.uk/varsity-2014" data-width="100" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
-					<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://felixonline.co.uk/varsity-2014" data-via="feliximperial" data-hashtags="imgvarsity">Tweet</a>
+					<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://felixonline.co.uk/varsity-2014" data-via="feliximperial" data-hashtags="impvarsity" data-text="I'm watching Varsity via the Felix and @stoictv live blog">Tweet</a>
 					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 				</div>
 			</div>
 		</div>
 		<div class="content row">
-			<div class="col-md-6 col-md-offset-1 feed">
+            <div class="col-md-6 mediabox">
+            <iframe width="100%" scrolling="no" height="480" align="middle" frameborder="0" border="0" allowfullscreen="true" src="http://galen.media.su.ic.ac.uk/stoic/stream/"></iframe>
+            </div>
+			<div class="col-md-6 feed">
 				<?php
 					// Output posts
 					foreach ($posts as $post) {
@@ -96,7 +99,7 @@
 
 	<script type="text/javascript">
 		$(function() {
-			LiveBlog.init('http://176.34.227.200:3000/varsity-2014', $('.feed'));
+			LiveBlog.init('http://sentry.letts.me:3000/varsity-2014', $('.feed'));
 		});
 	</script>
 
