ddafee6edcfbcc0a6a6b3551aac94c3311cd6458
Philip Kent <philip.kent@me.com>
2014-09-25 15:24:19 +0100

    Blog template

--- themes/2014/blog.php
+++ themes/2014/blog.php
@@ -7,37 +7,25 @@ $header = array(
 $theme->render('components/header', $header);
 ?>
 <!-- Blog wrapper -->
-<div class="container_12">
-	<!-- Sidebar -->
-	<div class="sidebar grid_4 push_8">
-		Sidebar
-		<?php 
-			/*
-			$theme->setSidebar(array(
-				'fbActivity', 
-				//'mediaBox',
-				'mostPopular'
-			));
-			$theme->renderSidebar();
-			*/
-		?>
-	</div>
-	<!-- End of sidebar -->
-
-	<!-- Blog container -->
-	<div class="grid_8 pull_4 <?php echo $blog->getSlug(); ?>">
-		<h2><?php echo $blog->getName(); ?></h2>
-		<div class="content">
-			<?php foreach($blog->getPosts() as $post) { ?>
-				<div class="blog-post" id="post<?php echo $post->getId(); ?>">
-					<p><?php echo $post->getContent(); ?></p>
-					<p>By: <?php echo $post->getAuthor()->getName(); ?></p>
-				</div>
-			<?php } ?>
+<!-- Page wrapper -->
+	<div class="row felix-pad-top">
+		<div class="medium-8 columns">
+			<h1><?php echo $blog->getName(); ?></h1>
+			<div class="content">
+				<?php foreach($blog->getPosts() as $post) { ?>
+					<div class="blog-post" id="post<?php echo $post->getId(); ?>">
+						<p><?php echo $post->getContent(); ?></p>
+						<p>By: <?php echo $post->getAuthor()->getName(); ?></p>
+					</div>
+				<?php } ?>
+			</div>
 		</div>
+		<div class="medium-4 columns">
+
+		</div>
+	<!-- End of page container -->
 	</div>
-	<!-- End of blog container -->
-</div>
-<!-- End of blog -->
+<!-- End of page -->
+
 <?php $timing->log('end of blog');?>
 <?php $theme->render('components/footer'); ?>
