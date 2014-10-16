c0cf4d841baba1d6941e633ca7611b8b78b1ef43
Philip Kent <philip.kent@me.com>
2014-10-16 17:49:39 +0100

    Fix article size in spinner

--- themes/2014/components/articlelist/featured_spinner.php
+++ themes/2014/components/articlelist/featured_spinner.php
@@ -4,7 +4,7 @@
 						<div class="felix-featured-image">
 						<?php if ($image = $article->getImage()) { ?>
 								<a href="<?php echo $article->getURL();?>">
-									<img alt="<?php echo $image->getTitle();?>" src="<?php echo $image->getURL();?>">
+									<img alt="<?php echo $image->getTitle();?>" src="<?php echo $image->getURL(1200);?>">
 								</a>
 						<?php } else { ?>
 								<a href="<?php echo $article->getURL();?>">
