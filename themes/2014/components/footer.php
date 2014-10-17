30116e8f323172c643d9ce73bb4994b10be2e82c
Philip Kent <philip.kent@me.com>
2014-10-17 14:54:08 +0100

    Add gosquared code

--- themes/2014/components/footer.php
+++ themes/2014/components/footer.php
@@ -60,6 +60,8 @@
 				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 			})();
+
+			<script> !function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push( arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0]; d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode. insertBefore(d,q)}(window,document,'script','_gs'); _gs('GSN-410478-T'); </script>
 		</script>
 	</body>
 </html>
