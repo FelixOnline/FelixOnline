00097f89d9af658032fd6e3fd9ac22e99fd18dc1
Philip Kent <philip.kent@me.com>
2015-01-04 19:38:17 +0000

    Rewrite AuthController to use Core and so rationalise the workflow for login

--- controllers/authController.php
+++ controllers/authController.php
@@ -15,50 +15,16 @@
 use FelixOnline\Exceptions;
 
 class AuthController extends BaseController {
-	private $session; // session id used in login
-
 	/*
 	 *
 	 */
 	function GET($matches) {
 		global $currentuser;
 		if(isset($_GET['session'])) { // catch login
-			$this->session = $_GET['session']; // Auth server's session ID
-			
 			// check if session is recent and that ip is the same
-			if($username = $this->checkLogin($this->session)) {
-				// Regenerate our session ID
-				$currentuser->resetToGuest();
-
-				// Remove any remaining cookies
-				$currentuser->removeCookie();
-
-				// Correct session ID - the one from the auth server is not
-				// the one on this server
-				$sql = $this->safesql->query("UPDATE login
-						SET session_id = '%s'
-						WHERE session_id='%s'
-						AND logged_in=0
-						AND ip='%s'
-						AND browser='%s'
-						AND valid=1
-						AND TIMESTAMPDIFF(SECOND,timestamp,NOW()) <= %i",
-						array(session_id(),
-							$this->session,
-							$_SERVER['REMOTE_ADDR'],
-							$_SERVER['HTTP_USER_AGENT'],
-							SESSION_LENGTH));
-
-				$this->session = session_id(); // The session ID auth is using
-											   // is now the one our session
-											   // has
-				$this->db->query($sql);
-
-				$currentuser->setUser($username);
-				$this->login();
-				
+			if($currentuser->restoreSession($_GET['session'])) {
 				if($_GET['remember'] == 'rememberme') {
-					$this->setCookie();
+					$currentuser->setCookie();
 				}
 				
 				Utility::redirect($_GET['goto']);
@@ -91,10 +57,8 @@ class AuthController extends BaseController {
 		if(isset($_POST['username']) && isset($_POST['password'])) {
 			try {
   			  if($this->authenticate($_POST['username'], $_POST['password'])) {
-					$currentuser->setUser($_POST['username']); // not needed
-					$this->logSession($_POST['username']);
-					$session = $currentuser->getSession();
-					$currentuser->updateDetails($_POST['username']); // update user details
+  			  		$currentuser->setUser($_POST['username']);
+  			  		$currentuser->createSession();
 
 					// comment like/dislike
 					if(isset($_POST['commenttype']) && isset($_POST['comment'])) {
@@ -108,12 +72,7 @@ class AuthController extends BaseController {
 					}
 					
 					// Close the session here, as we do not want lingering sessions on the auth server
-					$params = session_get_cookie_params();
-					setcookie(session_name(), '', time() - 42000,
-						$params["path"], $params["domain"],
-						$params["secure"], $params["httponly"]
-					); // Remove session ID
-					session_destroy(); // Remove all session data
+					$session = $currentuser->stashSession();
 	
 					Utility::redirect(STANDARD_URL.'login/', array(
 						'session' => $session,
@@ -164,188 +123,12 @@ class AuthController extends BaseController {
 	}
 
 	/*
-	 * Private: Log session in database
-	 */
-	private function logSession($username) {
-		global $currentuser;
-		$sql = $this->safesql->query("INSERT INTO `login` 
-									(
-										session_id,
-										ip,
-										browser,
-										user
-									) VALUES (%q)",
-									array(
-										array($currentuser->getSession(),
-												$_SERVER['REMOTE_ADDR'],
-												$_SERVER['HTTP_USER_AGENT'],
-												$username)));
-
-		return $this->db->query($sql);
-	}
-
-	/*
-	 * Private: Check login
-	 * Check login redirection 
-	 *
-	 * $session - session id
-	 *
-	 * Returns username of user if successful
-	 */
-	private function checkLogin($session) {
-		$sql = $this->safesql->query("SELECT 
-										user, 
-										TIMESTAMPDIFF(SECOND,timestamp,NOW()) AS timediff, 
-										ip, 
-										browser 
-									FROM `login` 
-									WHERE session_id='%s' 
-									AND valid=1 
-									AND logged_in=0 
-									ORDER BY timediff ASC 
-									LIMIT 1",
-									array($session));
-		$login = $this->db->get_row($sql);
-		if(
-			$login->timediff <= LOGIN_CHECK_LENGTH 
-			&& $login->ip == $_SERVER['REMOTE_ADDR'] 
-			&& $login->browser == $_SERVER['HTTP_USER_AGENT']
-		) {
-			return $login->user;
-		} else {
-			return false;
-		}
-	}
-
-	/*
-	 * Private: Login
-	 * Login current user
-	 */
-	private function login() {
-		global $currentuser;
-		$_SESSION['felix']['vname'] = $currentuser->getName();
-		//$_SESSION['felix']['name'] = $currentuser->getForename();
-		$_SESSION['felix']['uname'] = $currentuser->getUser();
-		$_SESSION['felix']['loggedin'] = true;
-		$this->setLoggedIn();
-		$this->destroyOldSessions();
-	}
-
-	/*
 	 * Private: Logout
 	 */
 	private function logout() {
 		global $currentuser;
-		$this->destroySession();
-		$currentuser->resetToGuest();
 
+		$currentuser->resetSession();
 		$currentuser->removeCookie();
 	}
-
-	/*
-	 * Private: Destroy the current session
-	 */
-	private function destroySession() {
-		global $currentuser;
-				
-		$sql = $this->safesql->query("UPDATE login
-				SET valid = 0
-				WHERE session_id='%s'
-				AND ip='%s'
-				AND browser='%s'
-				AND user='%s'",
-				array($currentuser->getSession(),
-					$_SERVER['REMOTE_ADDR'],
-					$_SERVER['HTTP_USER_AGENT'],
-					$currentuser->getUser()));
-
-		return $this->db->query($sql);
-	}
-
-	/*
-	 * Private: Destroy all user sessions
-	 */
-	private function destroySessions() {
-		global $currentuser;
-
-		$sql = $this->safesql->query("UPDATE `login` 
-				SET valid = 0
-				WHERE user='%s'",
-				array($currentuser->getUser()));
-				
-		return $this->db->query($sql);
-	}
-
-	/*
-	 * Private: Destroy old sessions
-	 */
-	private function destroyOldSessions() {
-		global $currentuser;
-		$sql = "DELETE FROM cookies 
-				WHERE UNIX_TIMESTAMP() > UNIX_TIMESTAMP(expires)
-		";
-		$this->db->query($sql);
-		$sql = $this->safesql->query("UPDATE `login` 
-				SET valid = 0
-				WHERE user='%s'
-				AND logged_in=0
-				OR TIMESTAMPDIFF(SECOND,timestamp,NOW()) > %i",
-				array($currentuser->getUser(),
-						SESSION_LENGTH));
-
-		return $this->db->query($sql);
-	}
-	
-	/*
-	 * Private: Get user from session
-	 * Get the user from the session id
-	 */
-	private function getUserFromSession($session) {
-		$sql = $this->safesql->query("SELECT user FROM login WHERE session_id='%s'", array($session));
-		return $this->db->get_var($sql);
-	}
-
-	/*
-	 * Private: Set session as logged in
-	 */
-	private function setLoggedIn() {
-		global $currentuser;
-		$sql = $this->safesql->query("UPDATE 
-										login 
-									SET 
-										logged_in=1 
-									WHERE 
-										user='%s' 
-										AND session_id = '%s'",
-									array($currentuser->getUser(), $this->session));
-
-		return $this->db->query($sql);
-	}
-
-	/*
-	 * Private: Create cookie
-	 */
-	private function setCookie() {
-		global $currentuser;
-
-		$hash = hash('sha256', mt_rand());
-
-		$expiry_time = time() + COOKIE_LENGTH;
-
-		setcookie('felixonline', $hash, $expiry_time, '/');
-		$sql = $this->safesql->query("INSERT INTO `cookies` 
-									(
-										hash,
-										user,
-										expires
-									) VALUES (
-										'%s',
-										'%s',
-										FROM_UNIXTIME(%i)
-									)",
-									array($hash, $currentuser->getUser(), $expiry_time));
-
-		return $this->db->query($sql);
-	}
-
 }
