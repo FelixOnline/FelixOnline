8c50940f930170b81c8815a2d2b309d9b97b9eb0
Philip Kent <philip.kent@me.com>
2015-01-07 23:33:05 +0000

    Display article author twitters - fixes #32

--- themes/2014/article.php
+++ themes/2014/article.php
@@ -205,6 +205,7 @@ $theme->render('components/header', $header);
 			<div class="medium-4 columns">
 				<div class="article-meta">
 					<div class="article-authors">By <b><?php echo Utility::outputUserList($article->getAuthors(), true); ?></b>.</div>
+					<div class="article-twitters"><?php echo Utility::outputUserTwitterList($article->getAuthors()); ?></div>
 					<div class="article-date">Published on <?php echo $article->getPublished() ? date('l F j, Y \a\t H:i', $article->getPublished()) : "<strong>Not Published</strong>";?>.</div>
 					<div class="article-comments"><span class="comment-count"><a href="<?php echo Utility::currentPageURL().'#commentHeader';?>"><?php echo $article->getNumComments().'</a></span> comment'.($article->getNumComments() != 1 ? 's' : '');?>.<?php if ($article->canComment($currentuser)) { ?> <a href="<?php echo Utility::currentPageURL().'#commentForm';?>">Post your own now</a>!<?php } ?></div>
 					<?php
