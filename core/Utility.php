9302bf73de553d60f9fe061524bd4043d8031233
Philip Kent <philip.kent@me.com>
2015-01-08 23:24:53 +0000

    Add utility (WIP) to get users responsibilities

--- core/Utility.php
+++ core/Utility.php
@@ -3,5 +3,37 @@
 use FelixOnline\Core;
 
 class Utility extends Core\Utility {
-	
+	static function getResponsibilities(Core\User $user) {
+		try {
+			$categories = $user->getCategories();
+		} catch(\Exception $e) {
+			throw $e;
+			$categories = array();
+		}
+
+		if(count($categories) == 0) {
+			return 'Contributor';
+		}
+
+		// change array into linked categories
+		foreach ($categories as $key => $cat) {
+			$full_array[$key] = $cat->getLabel();
+		}
+		// get last element
+		$last = array_pop($full_array);
+		// if it was the only element - return it
+		if (!count ($full_array))
+			return $last.' Editor';
+		$output = implode (', ', $full_array);
+		if($bold) {
+			$output .= ' and ';	
+		} else {
+			$output .= ' and ';
+		} 
+		$output .= $last;
+
+		$output .= ' Editors';
+
+		return $output;
+	}
 }
