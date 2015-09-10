dffb1b441885627bb2242952eb31036b1177269e
Philip Kent <philip.kent@me.com>
2015-09-10 21:43:29 +0100

    Define local as false if not set

--- inc/const.inc.php
+++ inc/const.inc.php
@@ -8,6 +8,7 @@
 	/* SYSTEM */
 	if(!defined('STANDARD_URL'))					define('STANDARD_URL','http://felixonline.co.uk/'); // standard site url
 	if(!defined('ADMIN_URL'))					 	define('ADMIN_URL','http://felixonline.co.uk/engine/'); // url of engine page
-	if(!defined('PRODUCTION_FLAG'))					define('PRODUCTION_FLAG', true); // if set to true css and js will be minified etc..
+	if(!defined('PRODUCTION_FLAG'))					define('PRODUCTION_FLAG', true); // if set to true css and js will be minified etc.
+	if(!defined('LOCAL'))							define('LOCAL', false); // if true then site is hosted locally - don't use pam_auth etc.
 	if(!defined('AUTHENTICATION_SERVER'))			define('AUTHENTICATION_SERVER','dougal.union.ic.ac.uk'); // authentication server
 	if(!defined('AUTHENTICATION_PATH'))				define('AUTHENTICATION_PATH','https://dougal.union.ic.ac.uk/media/felix/'); // authentication path
