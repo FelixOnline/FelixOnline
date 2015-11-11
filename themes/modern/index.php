7425e61a233b301ba0c196a03c062d1fa5b5fe6a
Philip Kent <philip.kent@me.com>
2015-11-11 22:09:59 +0000

    Rewrite CSS in Less format

--- themes/modern/index.php
+++ themes/modern/index.php
@@ -17,7 +17,7 @@ require_once(THEME_DIRECTORY.'/core/functions.php');
  */
 $this->resources = new Core\ResourceManager(
 	/* CSS files */
-	array('normalize.min.css', 'foundation.min.css', 'felix.css', 'slick.css', 'slick-theme.css', 'rrssb.css', 'glyphicons.css', 'glyphicons-social.css'), 
+	array('normalize.min.css', 'foundation.min.css', 'slick.css', 'slick-theme.css', 'rrssb.css', 'glyphicons.css', 'glyphicons-social.css', 'style.less'), 
 	/* JS files */
 	array('jquery.min.js', 'foundation.min.js', 'fastclick.js', 'jquery.cookie.js', 'jquery.placeholder.js', 'slick.min.js', 'jquery-visible.js', 'script.js', 'rrssb.min.js')
 );
