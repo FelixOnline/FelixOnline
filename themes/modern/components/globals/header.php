ce46c29cea812cce2b03be6628c4a103b7b47dae
Philip Kent <philip.kent@me.com>
2015-11-19 19:05:28 +0000

    Add cookie consent

--- themes/modern/components/globals/header.php
+++ themes/modern/components/globals/header.php
@@ -38,6 +38,14 @@
 				echo 'Felix Online - The student voice of Imperial College London';
 			} ?> 
 		</title>
+
+		<!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
+		<script type="text/javascript">
+			window.cookieconsent_options = {"message":"We use cookies to improve your experience on our website. By browsing this website, you agree to our use of cookies.","dismiss":"Got it!","learnMore":"More info","link":null,"theme":"dark-bottom"};
+		</script>
+
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.9/cookieconsent.min.js"></script>
+		<!-- End Cookie Consent plugin -->
 	</head>
 	<body>
 		<div id="fb-root"></div>
