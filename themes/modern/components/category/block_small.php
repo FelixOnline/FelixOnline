901a444a8b3be74eeefea1615a1d282f648ceeb7
Philip Kent <philip.kent@me.com>
2015-12-01 21:58:10 +0000

    Make small article block comment links clickable

--- themes/modern/components/category/block_small.php
+++ themes/modern/components/category/block_small.php
@@ -3,6 +3,6 @@
                 <div class="small"<?php if($equalizer): ?> data-equalizer-watch="<?php echo $equalizer; ?>"<?php endif; ?>>
                   <div class="article-title"><a href="<?php echo $article->getUrl(); ?>"><?php echo $article->getTitle(); ?></a></div>
                   <span class="article-time"><span class="glyphicons glyphicons-clock"></span><?php echo Utility::getRelativeTime($article->getPublished()); ?></span>
-                  <?php if($comments > 0): ?><span class="article-inline-comments"><span class="glyphicons glyphicons-comments"></span>&nbsp;<?php echo $comments; ?></span><?php endif; ?>
+                  <?php if($comments > 0): ?><span class="article-inline-comments"><a href="<?php echo $article->getUrl(); ?>#commentHeader"><span class="glyphicons glyphicons-comments"></span>&nbsp;<?php echo $comments; ?></a></span><?php endif; ?>
                 </div>
               </div>
