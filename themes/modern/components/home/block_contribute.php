e8db186f1ef6bb1dc0eec257f06e2b8197f7f5af
Philip Kent <philip.kent@me.com>
2015-12-02 21:59:30 +0000

    Tidy up the contribute box on the front page

--- themes/modern/components/home/block_contribute.php
+++ themes/modern/components/home/block_contribute.php
@@ -1,5 +1,5 @@
             <div class="contact-info-box info-box">
-              <h1>Contribute to Felix</h1>
-              <p>It's easy and everyone is welcome.</p>
-              <div class="text-center"><button class="button small radius">Get involved</button></div>
+              <h1>Get involved</h1>
+              <p>It's easy to get involved with Felix, and everyone is welcome.</p>
+              <div class="text-center"><button class="button small radius">Find out how</button></div>
             </div>
