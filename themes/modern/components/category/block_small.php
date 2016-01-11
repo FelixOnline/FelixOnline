c317a03b8648cef20bdfd84f7637bdab1c6542cb
Philip Kent <philip.kent@me.com>
2016-01-11 21:22:11 +0000

    Add video icon to article blocks if they have a video attached

--- themes/modern/components/category/block_small.php
+++ themes/modern/components/category/block_small.php
@@ -4,5 +4,6 @@
                   <div class="article-title"><a href="<?php echo $article->getUrl(); ?>"><?php echo $article->getTitle(); ?></a></div>
                   <span class="article-time"><span class="glyphicons glyphicons-clock"></span><?php echo Utility::getRelativeTime($article->getPublished()); ?></span>
                   <?php if($comments > 0): ?><span class="article-inline-comments"><a href="<?php echo $article->getUrl(); ?>#commentHeader"><span class="glyphicons glyphicons-comments"></span>&nbsp;<?php echo $comments; ?></a></span><?php endif; ?>
+                  <?php if($article->getVideoUrl()): ?><span class="article-inline-comments"><span class="glyphicons glyphicons-facetime-video"></span></span><?php endif; ?>
                 </div>
               </div>
