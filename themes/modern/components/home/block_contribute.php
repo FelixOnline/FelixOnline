b41fbb2f7334ba94b8e96e3cdd0107b006a48972
Philip Kent <philip.kent@me.com>
2016-01-17 18:20:00 +0000

    Fix a broken link

--- themes/modern/components/home/block_contribute.php
+++ themes/modern/components/home/block_contribute.php
@@ -19,7 +19,7 @@
             <div class="contact-info-box info-box">
               <h1>Get involved</h1>
               <p>It's easy to get involved with Felix, and everyone is welcome.</p>
-              <div class="text-center"><button class="button small radius">Find out how</button></div>
+              <div class="text-center"><a class="button small radius" href="<?php echo STANDARD_URL; ?>/contribute">Find out how</a></div>
             </div>
 <?php
 		}
