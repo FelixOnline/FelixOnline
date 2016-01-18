363809a89bdc80c5c84418d98f697f74dc87774f
Philip Kent <philip.kent@me.com>
2016-01-18 19:49:22 +0000

    delete old fb stuff

--- themes/modern/js/script.js
+++ themes/modern/js/script.js
@@ -5,13 +5,6 @@
 $(document).ready(function() {
 	/* Facebook */
 
-	(function(d, s, id) {
-		var js, fjs = d.getElementsByTagName(s)[0];
-		if (d.getElementById(id)) return;
-		js = d.createElement(s); js.id = id;
-		js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=200482590030408";
-		fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
 
 	/* Comment Binders - logged out */
 	$('.logout-like').bind('click', function() {
