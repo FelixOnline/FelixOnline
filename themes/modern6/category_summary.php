5e7240e6db73111ced67b836b913f7d43dee5767
Philip Kent <philip.kent@me.com>
2016-03-06 18:26:03 +0000

    Fix the category summary page

--- themes/modern6/category_summary.php
+++ themes/modern6/category_summary.php
@@ -10,48 +10,32 @@ $header = array(
 $theme->render('components/globals/header', $header);
 
 ?>
-<?php if($category->getChildren()): ?>
-	<div class="row full-width">
-		<div class="small-12 columns">
-			<?php $theme->render('components/category/block_subnav'); ?>
-		</div>
-	</div>
-<?php endif; ?>
-	<div class="row full-width">
-		<div class="small-12 large-8 columns breadcrumb-float-top">
-			<?php $theme->render('components/helpers/breadcrumbs', array('origin' => $category, 'type' => 'category')); ?>
+	<div class="row main-row top-row">
+		<div class="small-12 medium-8 columns ">
 			<h1 class="breadcrumb-after"><?php echo $category->getLabel(); ?></h1>
 		</div>
-		<div class="small-12 large-4 columns text-right">
+		<div class="small-12 medium-4 columns text-right">
 			<?php $theme->render('components/helpers/block_advert', array('sidebar' => true, 'article' => false, 'section' => $category)); ?>
 		</div>
 	</div>
 <?php foreach($children as $child): ?>
-	<div class="row <?php echo $child->getCat(); ?>">
+	<div class="row main-row <?php echo $child->getCat(); ?>">
 		<div class="small-12 columns">
 			<div class="bar-text"><a href="<?php echo $child->getURL(); ?>"><?php echo $child->getLabel(); ?></a></div>
 		</div>
 	</div>
-	<div class="row full-width" data-equalizer="<?php echo $child->getCat(); ?>">
-<?php for($i = 0; $i < count($articles[$child->getCat()]); $i++): ?>
-<?php
-	if($i > 0 && ($i + 1) % 4 == 0) {
-		$end = ' end';
-	} else {
-		$end = '';
-	}
-?>
-	<div class="small-12 large-3 columns<?php echo $end; ?>">
+	<div class="row small-up-1 medium-up-4 main-row top-row">
+<?php foreach($articles[$child->getCat()] as $article): ?>
+	<div class="columns">
 	<?php
 		$theme->render('components/category/block_normal', array(
-			'article' => $articles[$child->getCat()][$i],
-			'equalizer' => $child->getCat(),
+			'article' => $article,
 			'show_category' => false,
 			'headshot' => false
 		));
 	?>
 	</div>
-<?php endfor; ?>
+<?php endforeach; ?>
 	</div>
 <?php endforeach; ?>
 <?php
