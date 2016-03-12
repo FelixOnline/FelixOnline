64c05435850789d5ac3adfab12998ffe74471530
Philip Kent <philip.kent@me.com>
2016-03-12 22:12:25 +0000

    Make the topic overview more resilient to topics with no articles in

--- themes/modern6/components/article/topic_block.php
+++ themes/modern6/components/article/topic_block.php
@@ -2,7 +2,13 @@
 								<div class="article-topic" style="background-image: url('<?php echo $topic->getImage()->getUrl(); ?>');">
 									<div class="article-topic-info">
 										<h2><?php echo $topic->getName(); ?></h2>
-										<span class="topic-start topic-date"><?php echo date('F Y', $topic->getStartDate()); ?></span> <span class="glyphicons glyphicons-chevron-right"></span> <span class="topic-start topic-date"><?php echo date('F Y', $topic->getEndDate()); ?></span>
+										<?php
+											try {
+										?>
+											<span class="topic-start topic-date"><?php echo date('F Y', $topic->getStartDate()); ?></span> <span class="glyphicons glyphicons-chevron-right"></span> <span class="topic-start topic-date"><?php echo date('F Y', $topic->getEndDate()); ?></span>
+										<?php
+											} catch (\Exception $e) { }
+										?>
 									</div>
 								</div>
 							</a>
