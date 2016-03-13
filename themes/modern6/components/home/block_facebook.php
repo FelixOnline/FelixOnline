2e612088265457a1e1d8720970ff7ff7dd419056
Philip Kent <philip.kent@me.com>
2016-03-13 23:07:58 +0000

    Hide Facebook box on small screens

--- themes/modern6/components/home/block_facebook.php
+++ themes/modern6/components/home/block_facebook.php
@@ -1,3 +1,3 @@
-<?php if($well): ?><div class="panel"><?php endif; ?>
-	<div class="fb-page" data-href="https://www.facebook.com/feliximperial" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/feliximperial"><a href="https://www.facebook.com/feliximperial">Felix</a></blockquote></div></div>
+<?php if($well): ?><div class="panel show-for-medium"><?php endif; ?>
+	<div class="fb-page show-for-medium" data-href="https://www.facebook.com/feliximperial" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/feliximperial"><a href="https://www.facebook.com/feliximperial">Felix</a></blockquote></div></div>
 <?php if($well): ?></div><?php endif; ?>
